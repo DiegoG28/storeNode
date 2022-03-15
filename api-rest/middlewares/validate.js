5d1f822995da7611a4f294c84b169626647efefb
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-03-15 02:07:27 -0500

    Creación de esquemas y middleware para validar estos esquemas

--- api-rest/middlewares/validate.js
+++ api-rest/middlewares/validate.js
@@ -1,17 +1,13 @@
 const StatusCodeError = require('../modules/statusCodeError');
 
 function validateInputs(req, res, next) {
-	try {
-		const body = req.body;
-		if (!body.nombre || !body.apellido) {
-			throw new StatusCodeError(400, 'El campo nombre y apellido son necesarios');
-		} else {
-			res.locals.body = body;
-			next();
-		}
-	} catch (error) {
-		next(error);
-	}
+   const body = req.body;
+   if (!body.nombre || !body.apellido) {
+      const error = new StatusCodeError(400, 'El campo nombre y apellido son necesarios');
+      next(error);
+   }
+   res.locals.body = body;
+   next();
 }
 
 module.exports = { validateInputs };

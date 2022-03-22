000fe0a56e4f347fbe9f260f5b5f09030e5f05f9
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-03-21 22:26:35 -0500

    Se habilitó CORS únicamente para localhost.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
@@ -7,6 +8,16 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://127.0.0.1:5500', 'http://localhost'];
+const options = {
+   origin: (origin, callback) => {
+      (whitelist.includes(origin) || !origin)
+      ? callback(null, true)
+      : callback(new Error('This origin is not allowed'));
+   }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
    res.send('My server in express');

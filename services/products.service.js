1208837e43c4167fa9b891457b1ed51cf2dfa277
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-04-05 00:39:20 -0500

    Uso de variables de entorno y URI para realizar la conexión a la base de datos

--- services/products.service.js
+++ services/products.service.js
@@ -1,75 +1,34 @@
-const faker = require('community-faker');
 const boom = require('@hapi/boom');
+
+const pool = require('../libs/postgres.pool');
 
 class ProductsService {
 
    constructor() {
-      this.products = [];
-      this.generate();
-   }
-
-   async generate() {
-      const size = 100;
-      for (let i = 0; i < size; i++) {
-         this.products.push({
-            id: faker.datatype.uuid(),
-            name: faker.commerce.productName(),
-            price: parseInt(faker.commerce.price(), 10),
-            image: faker.image.imageUrl(),
-            isBlock: faker.datatype.boolean()
-         });
-      }
+      this.pool = pool;
+      this.pool.on('error', (err) => console.log(err));
    }
 
    async create(data) {
-      const newProduct = {
-         id: faker.datatype.uuid(),
-         ...data
-      };
-      this.products.push(newProduct);
-      return newProduct;
+
    }
 
    async find() {
-      return new Promise((resolve, reject) => {
-         setTimeout(() => {
-            resolve(this.products);
-         }, 3000);
-      })
+      const queryText = 'SELECT * FROM tasks';
+      const res = await this.pool.query(queryText);
+      return res.rows;
    }
 
    async findOne(id) {
-      const product = this.products.find(item => item.id === id);
-      if (!product) {
-         throw boom.notFound('product not found');
-      }
-      if(product.isBlock) {
-         throw boom.conflict('product is blocked');
-      }
-      return product;
+
    }
 
    async update(id, changes) {
-      const index = this.products.findIndex(item => item.id === id);
-      if (index === -1) {
-         throw boom.notFound('product not found');
-      }
-      const product = this.products[index];
-      const productChanged = {
-         ...product,
-         ...changes
-      }
-      this.products[index] = productChanged;
-      return productChanged;
+
    }
 
    async delete(id) {
-      const index = this.products.findIndex(item => item.id === id);
-      if (index === -1) {
-         throw boom.notFound('product not found');
-      }
-      this.products.splice(index, 1);
-      return { id };
+
    }
 
 }
--- services/users.service.js
+++ services/users.service.js
@@ -1,75 +1,34 @@
-const faker = require('community-faker');
 const boom = require('@hapi/boom');
 
-const getConnection = require('./../libs/postgres');
+const pool = require('../libs/postgres.pool');
 
 class UsersService {
 
    constructor() {
-      this.users = [];
-      this.generate();
-   }
-
-   async generate() {
-      const size = 100;
-      for (let i = 0; i < size; i++) {
-         this.users.push({
-            id: faker.datatype.uuid(),
-            name: faker.name.firstName(),
-            last: faker.name.lastName(),
-            gender: faker.name.gender(),
-            isBlock: faker.datatype.boolean()
-         });
-      }
+      this.pool = pool;
+      this.pool.on('error', (err) => console.log(err));
    }
 
    async create(data) {
-      const newUser = {
-         id: faker.datatype.uuid(),
-         ...data
-      };
-      this.users.push(newUser);
-      return newUser;
+
    }
 
    async find() {
-      const client = await getConnection();
-      const res = await client.query('SELECT * FROM public.tasks');
+      const queryText = 'SELECT * FROM tasks';
+      const res = await this.pool.query(queryText);
       return res.rows;
    }
 
    async findOne(id) {
-      const user = this.users.find(item => item.id === id);
-      if (!user) {
-         throw boom.notFound('user not found');
-      }
-      if(user.isBlock){
-         throw boom.conflict('user is blocked');
-      }
-      return user;
+
    }
 
    async update(id, changes) {
-      const index = this.users.findIndex(item => item.id === id);
-      if (index === -1) {
-         throw boom.notFound('user not found');
-      }
-      const user = this.users[index];
-      const userChanged = {
-         ...user,
-         ...changes
-      };
-      this.users[index] = userChanged;
-      return userChanged;
+
    }
 
    async delete(id) {
-      const index = this.users.findIndex(item => item.id === id);
-      if (index === -1) {
-         throw boom.notFound('user not found');
-      }
-      this.users.splice(index, 1);
-      return { id };
+
    }
 }
 
